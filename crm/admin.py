75c63365436ed60d296fd5c33d1ed1a81ea1ce73
Stuti Raut <stutsraut1@gmail.com>
2018-09-30 12:37:45 -0500

    Added services and product table

--- crm/admin.py
+++ crm/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, Service, Product
+
 
 class CustomerList(admin.ModelAdmin):
     list_display = ( 'cust_name', 'organization', 'phone_number' )
@@ -8,4 +9,20 @@ class CustomerList(admin.ModelAdmin):
     search_fields = ('cust_name', )
     ordering = ['cust_name']
 
-admin.site.register(Customer)
+class ServiceList(admin.ModelAdmin):
+    list_display = ( 'cust_name', 'service_category', 'setup_time')
+    list_filter = ( 'cust_name', 'setup_time')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+class ProductList(admin.ModelAdmin):
+    list_display = ( 'cust_name', 'product', 'pickup_time')
+    list_filter = ( 'cust_name', 'pickup_time')
+    search_fields = ('cust_name', )
+    ordering = ['cust_name']
+
+
+
+admin.site.register(Customer, CustomerList)
+admin.site.register(Service, ServiceList)
+admin.site.register(Product, ProductList)
